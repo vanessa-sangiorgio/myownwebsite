b2a973f791ac4d743178ad6f26514e8e9695f39d
Vanessa Sangiorgio <vanessa.sangiorgio@yahoo.co.uk>
2016-07-21 20:16:31 +0200

    Added animation to slider by js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
 $(document).ready(function(){
 
-$("#card").click(function(){
-  $("#front").animate({height: 'toggle'}, "slow");
-  $("#back").animate({height: 'show'}, "slow");
-});
-
-$(".card2").click(function(){
-  $("#front-draw").animate({height: 'toggle'}, "slow");
-  $("#back-draw").animate({height: 'show'}, "slow");
-});
-
  $(".sidebar-btn").click(function(){
    $("#sidebar").toggleClass("visible");
  });
 
-$("#skill").click(function(){
+/*$("#skill").click(function(){
   $("#div-skill").toggleClass("showing");
+});*/
+
+$("#skill").click(function(){
+$("#skill-slider").slideToggle("slow");
+});
+
+$("#animals").click(function(){
+  $("#card").toggleClass("flipped");
+});
+
+$("#environment").click(function(){
+  $("#card2").toggleClass("flipped");
+});
+
+$("#drawing").click(function(){
+  $("#card3").toggleClass("flipped");
+});
+
+//SLIDER SKILLS
+var currentIndex = 0,
+items = $(".list-slides .slide"),
+itemAmt = items.length;
+
+function cycleItems() {
+  var item = $(".list-slides .slide").eq(currentIndex);
+  items.hide();
+  item.css('display', 'inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 6000);
+
+$(".next").click(function() {
+  clearInterval(autoSlide);
+  currentIndex +=1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$(".prev").click(function() {
+  clearInterval(autoSlide);
+  currentIndex -=1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
 });
 
 });
