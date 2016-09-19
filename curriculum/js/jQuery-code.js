f250037bfeecd5873f7fe2332ad9a62759a11bc8
Vanessa Sangiorgio <vanessa.sangiorgio@yahoo.co.uk>
2016-09-19 21:08:08 +0200

    Add form

--- curriculum/js/jQuery-code.js
+++ curriculum/js/jQuery-code.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-  $(".sidebar-btn").click(function(){
-  $("#sidebar").toggleClass("visible");
+   $(".sidebar-btn").click(function(){
+     $("#sidebar").toggleClass("visible");
+   });
+
+   $(".sidebar-btn").click(function(){
+     $(".sidebar-btn").toggleClass("change");
+   });
+
+$(".backtotop").click(function(event){
+  event.preventDefault();
+  $("html, body").animate({
+    scrollTop:0
+  }, 800);
 });
+
+$(".contactMe").click(function() {
+  $(".modal").addClass("isactive");
+});
+
+$("#contact").click(function() {
+  $(".modal").addClass("isactive");
+});
+
+$(".fa-envelope").click(function() {
+  $(".modal").addClass("isactive");
+});
+
+$(".fa-times").click(function() {
+  $(".modal").removeClass("isactive");
+});
+
 });
