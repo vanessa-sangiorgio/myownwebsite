6207e65a0bfa273a00b1eee2964bcfc9f7377894
Vanessa Sangiorgio <vanessa.sangiorgio@yahoo.co.uk>
2016-09-19 20:57:56 +0200

    Add form

--- arcade-game/js/jQuery-code.js
+++ arcade-game/js/jQuery-code.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
    $(".sidebar-btn").click(function(){
      $(".sidebar-btn").toggleClass("change");
    });
-  
+
 
 $(".backtotop").click(function(event){
   event.preventDefault();
@@ -14,4 +14,27 @@ $(".backtotop").click(function(event){
     scrollTop:0
   }, 800);
 });
+
+
+//modal//
+$("#contact").click(function() {
+  $(".modal").addClass("isactive");
+});
+
+$(".fa-envelope").click(function() {
+  $(".modal").addClass("isactive");
+});
+
+$(".fa-times").click(function() {
+  $(".modal").removeClass("isactive");
+});
+
+$("#mockup").click(function() {
+  $(".modal-mockup").addClass("isactive");
+});
+
+$("#exit").click(function() {
+  $(".modal-mockup").removeClass("isactive");
+});
+
 });
